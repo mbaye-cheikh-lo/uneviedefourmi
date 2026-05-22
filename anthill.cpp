bc9fac05c806b01652fcba48d9e2b068771fee23
childebert bouaichi <childebert.bouaichi@laplateforme.io>
2026-05-22 10:37:18 +0200

    class for create and manipulate the Anthill

--- anthill.cpp
+++ anthill.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include "anthill.hpp"
 using namespace std;
-Anthill::Anthill {
-public:
-  int room;
-  int tunnels;
-  int start;
-  int end;
-public:
-  int add_room()
-  int add_tunnel()
-  int create_ants()
+Anthill::Anthill() {
+
+  
+}
+
+void  Anthill::add_room()
+{
+  
+}
+void Anthill::add_tunnel()
+{
+  
+}
+void Anthill::create_ants()
+{
   
 }
